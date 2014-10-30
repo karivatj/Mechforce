5736885690a18d5656d108ded3da342c8e268505
Kari Vatjus-Anttila <kari.vatjusanttila@gmail.com>
2014-10-30 16:21:31 +0200

    Changed to GLSL 1.3 because apparently Intel HD3000 does not support GLSL 3.3

--- bin/Data/fshader.glsl
+++ bin/Data/fshader.glsl
@@ -1,6 +1,5 @@
-#version 330 core
+#version 130
 out vec3 color;
- 
 void main(){
     color = vec3(1,0,0);
 }
--- bin/Data/vshader.glsl
+++ bin/Data/vshader.glsl
@@ -1,8 +1,7 @@
-#version 330 core
-
-layout(location = 0) in vec3 vertexPosition_modelspace;
-
+#version 130
+//layout(location = 0) in 
+vec3 vertexPosition_modelspace;
 void main(){
     gl_Position.xyz = vertexPosition_modelspace;
     gl_Position.w = 1.0;
- }
+}
